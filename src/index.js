53294b251358070b6dd94c4b4195ff977821870e
韩小庚 <2068470260@qq.com>
2020-05-20 02:15:45 +0800

    add taobao component

--- src/index.js
+++ src/index.js
@@ -6,14 +6,16 @@ import './assets/base.css'; // 02 全局引入公共样式
 
 import BaiDu from './components/BaiDu.js'  //import 必须在其它所有业务代码前面（eslint 暴出）,异步组件导入了,也不能插在其他导入的东西前面.
 import DataAjax from './components/DataAjax.js'
+import TaoBap from './components/TaoBao.js'
+import TaoBao from './components/TaoBao.js';
 registerServiceWorker();
 
 ReactDOM.render(
   <div>
     <BaiDu />
     <DataAjax/>
-  </div>
-  ,
+    <TaoBao/>
+  </div>,
   document.getElementById('root')
 );
 
